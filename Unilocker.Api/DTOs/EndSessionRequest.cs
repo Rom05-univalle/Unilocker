6ab2fdf26dca371eb5cc21e91d3c06b03a28c848
Rom05-univalle <ghr0034560@est.univalle.edu>
2025-11-23 17:32:45 -0400

    session management done

---
+++ Unilocker.Api/DTOs/EndSessionRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Unilocker.Api.DTOs;
+
+public class EndSessionRequest
+{
+    [Required(ErrorMessage = "El método de cierre es requerido")]
+    [RegularExpression("^(Normal|Forced|Timeout|Administrative)$",
+        ErrorMessage = "EndMethod debe ser: Normal, Forced, Timeout o Administrative")]
+    public string EndMethod { get; set; } = null!;
+}
