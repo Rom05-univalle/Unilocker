d972c9680f3b2a08f85da1c80b7ddf45e2474565
Rom05-univalle <ghr0034560@est.univalle.edu>
2025-11-27 09:18:15 -0400

    services and models on client done

---
+++ Unilocker.Client/Models/SessionResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unilocker.Client.Models
+{
+    internal class SessionResponse
+    {
+    }
+}
